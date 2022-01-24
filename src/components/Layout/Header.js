73ac0669df5954c9dba0a505edfa380c55fe5cce
theeastwindz0 <developerhimanshuchauhan@gmail.com>
2022-01-24 13:11:19 +0530

    Added Header BUtton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React,{Fragment} from 'react'
 import classes from './Header.module.css'
 import meals from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
 
 const Header=()=>{
     return(
     <React.Fragment>
     <header className={classes.header}>
         <h1>FOOD APP</h1>
-        <button>CART</button>
+        <HeaderCartButton/>
     </header>
     <div className={classes['main-image']}>
         <img src={meals} alt='Table Full of Food'/>
