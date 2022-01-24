69f66bdff58beb58e4a04b8443901e7bcf35a0dc
theeastwindz0 <developerhimanshuchauhan@gmail.com>
2022-01-24 13:36:07 +0530

    Added Meals Summary & Available Meals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
   <React.Fragment>
   <Header/>
+  <Meals/>
   </React.Fragment>
   );
 }
