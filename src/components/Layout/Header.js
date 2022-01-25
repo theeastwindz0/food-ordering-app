223f4135ce72bad5d801936fd0c39b1b020207bf
theeastwindz0 <developerhimanshuchauhan@gmail.com>
2022-01-25 12:21:16 +0530

    Added useEffect (Bump Effect on Adding Item)

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,24 +1,18 @@
-import React,{useContext} from 'react'
+import React from 'react'
 import classes from './Header.module.css'
 import meals from '../../assets/meals.jpg'
 import HeaderCartButton from './HeaderCartButton'
 import logo from '../../assets/logo.png'
-import CartContext from '../../store/CartContext'
 const Header=(props)=>{
 
-    const cartCtx=useContext(CartContext);
-
-    /* reduce method which transform array of datat into a single value*/
-    const totalItems=cartCtx.items.reduce((curNumber,item)=>{
-        return curNumber+item.amount
-    },0)
+    
     return(
     <React.Fragment>
     <header className={classes.header}>
         <div className={classes.logo}>
         <img  src={logo} alt="Logo"/>
         </div>
-        <HeaderCartButton totalItems={totalItems} onClick={props.onShowCart} />
+        <HeaderCartButton  onClick={props.onShowCart} />
     </header>
     <div className={classes['main-image']}>
         <img src={meals} alt='Table Full of Food'/>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/CartContext";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+
+  const cartCtx=useContext(CartContext);
+
+  //Object destructing to pull out the items only
+  const {items}=cartCtx;
+
+    /* reduce method which transform array of datat into a single value*/
+    const totalItems=items.reduce((curNumber,item)=>{
+        return curNumber+item.amount
+    },0)
+
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const btnClasses = `${classes.button} ${btnIsHighlighted? classes.bump:''}`;
+
+  useEffect(() => {
+    if(
+      items.length===0)
+    {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer=setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return()=>{
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{props.totalItems}</span>
+      <span className={classes.badge}>{totalItems}</span>
     </button>
   );
 };
