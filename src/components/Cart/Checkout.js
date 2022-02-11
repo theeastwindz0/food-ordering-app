b60b8cb23c621a696e463b1471ac57d27a8cdabe
theeastwindz0 <developerhimanshuchauhan@gmail.com>
2022-02-11 11:14:18 +0530

    Added Form validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,34 +1,90 @@
-import React from 'react'
-import classes from './Checkout.module.css'
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
+
+const isEmpty = (value) => value.trim() === "";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
 
-    const confirmHandler=(event)=>
-    {
-        event.preventDefault();
+  const [formValidity, setFormValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
+  const confirmHandler = (event) => {
+
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = !isEmpty(enteredPostalCode);
+
+    setFormValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      postalCode: enteredPostalCodeIsValid,
+      city: enteredCityIsValid,
+    });
+
+    const formIsValid =
+      enteredCityIsValid &&
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (formIsValid) {
+      return;
     }
-  return (
-    <form onSubmit={confirmHandler}>
-        <div className={classes.control}>
-            <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name'/>
-        </div>
-        <div className={classes.control}>
-            <label htmlFor='street'>Street</label>
-            <input type='text' id='street'/>
-        </div>
-        <div className={classes.control}>
-            <label htmlFor='postal'>Postal Code</label>
-            <input type='text' id='postal'/>
-        </div>
-        <div className={classes.control}>
-            <label htmlFor='city'>City</label>
-            <input type='text' id='city'/>
-        </div>
-        <button type='button' onClick={props.onCancel}>Cancel</button>
-        <button>Confirm</button>
-    </form>
-  )
-}
+  };
 
-export default Checkout
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={`${classes.control} ${
+          formValidity.name ? '' : classes.invalid
+      }`}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formValidity.name && <p>Please entere a valid name !</p>}
+      </div>
+      <div className={`${classes.control} ${
+          formValidity.street ? '' : classes.invalid
+      }`}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formValidity.street && <p>Please entere a valid street !</p>}
+      </div>
+      <div className={`${classes.control} ${
+          formValidity.postalCode ? '' : classes.invalid
+      }`}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formValidity.postalCode && <p>Please entere a valid postal code !</p>}
+      </div>
+      <div className={`${classes.control} ${
+          formValidity.city ? '' : classes.invalid
+      }`}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formValidity.city && <p>Please entere a valid city !</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
